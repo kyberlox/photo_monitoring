151b5c217e0e9d696d16359aa3342e22dd655a68
kyberlox <alexxx.timofeev.00@gmail.com>
2026-04-03 10:41:35 +0400

    form data

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,4 +1,4 @@
-dfrom .locations import router as locations_router
+from .locations import router as locations_router
 from .photos import router as photos_router
 
 __all__ = ["locations_router", "photos_router"]
